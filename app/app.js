4b54c886e2d9abb08b0bd09141fbf7c6346f0c00
AmrDhaliwal <amritdhaliwalz@gmail.com>
2021-05-19 01:01:24 -0700

    Uploaded the completed front-end for the project and added the skeleton files for the express server.

---
+++ app/app.js
@@ -0,0 +1,14 @@
+// Backend
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req, res) {
+  res.send("Hello, World");
+});
+
+
+app.listen(3000, function() {
+  console.log("Server is up");
+});
